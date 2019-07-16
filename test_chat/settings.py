7aedcd51c34aabb0e5f9a8b795315f42cc070cf6
Dy0mar <kurtudins@gmail.com>
2019-07-16 19:08:09 +0300

    add static files and some settings

--- test_chat/settings.py
+++ test_chat/settings.py
@@ -119,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
+STATICFILES_DIRS = (
+    os.path.join(STATIC_ROOT, 'static'),
+)
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
