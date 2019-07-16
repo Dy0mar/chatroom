faf849d24e92bcd4aa512b0fbe1ded88f0ea1383
Dy0mar <kurtudins@gmail.com>
2019-07-16 19:52:01 +0300

    add Model ChatMessage

--- chatroom/models.py
+++ chatroom/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class ChatMessage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField(max_length=3000)
+    message_html = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.message
